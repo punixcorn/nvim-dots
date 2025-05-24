ddbd6d0598ca3c7914a1754f1b15e0b465f963c4
potato <103060805+punixcorn@users.noreply.github.com>
2025-05-24 23:58:03 +0100

    Update others.lua

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -543,18 +543,20 @@ return {
 		},
 		config = function()
 			require("markview").setup({
-				modes = { "n", "no", "c" }, -- Change these modes
-				-- to what you need
+				preview = {
+				 modes = { "n", "no", "c" }, -- Change these modes
+				 -- to what you need
 
-				hybrid_modes = { "n" }, -- Uses this feature on
-				-- normal mode
+				 hybrid_modes = { "n" }, -- Uses this feature on
+				 -- normal mode
 
-				-- This is nice to have
-				callbacks = {
-					on_enable = function(_, win)
-						vim.wo[win].conceallevel = 2
-						vim.wo[win].concealcursor = "c"
-					end,
+				 -- This is nice to have
+				 callbacks = {
+					 on_enable = function(_, win)
+						 vim.wo[win].conceallevel = 2
+						 vim.wo[win].concealcursor = "c"
+					 end,
+				 },
 				},
 			})
 		end,
